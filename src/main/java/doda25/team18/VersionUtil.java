b91a1a0bc8c4c93bd9c10370657b69b4a97736a7
Jan Kuhta <jan.kuhta@gmail.com>
2025-11-19 22:07:13 +0100

    fix(f7): update versionUtil to not use hardcoded version

--- src/main/java/doda25/team18/VersionUtil.java
+++ src/main/java/doda25/team18/VersionUtil.java
@@ -2,6 +2,7 @@ package doda25.team18;
 
 public class VersionUtil {
     public static String getVersion() {
-        return "1.0.0";
+        String version = VersionUtil.class.getPackage().getImplementationVersion();
+        return version != null ? version : "unknown";
     }
 }
